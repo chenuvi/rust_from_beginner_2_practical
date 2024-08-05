8c3431af2e54b86393336dbb770eeecbede5713d
kunchen16 <kunchen16@iflytek.com>
2024-08-05 10:59:05 +0800

    Feat: global 404 & module handlers

---
+++ 21-axumapp/src/handlers.rs
@@ -0,0 +1,77 @@
+use axum::{
+    extract::Query,
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    Form, Json,
+};
+use serde::Deserialize;
+use serde_json::json;
+pub async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+pub struct Params {
+    foo: i32,
+    bar: String,
+    aa: Option<i32>,
+}
+
+pub async fn query(Query(params): Query<Params>) -> impl IntoResponse {
+    tracing::debug!("query params {:?}", params);
+    Html("<h3>Test query</h3>")
+}
+
+pub async fn show_form() -> Html<&'static str> {
+    Html(
+        r#"
+        <!doctype html>
+        <html>
+            <head></head>
+            <body>
+                <form action="/form" method="post">
+                    <label for="name">
+                        Enter your name:
+                        <input type="text" name="name">
+                    </label>
+
+                    <label>
+                        Enter your email:
+                        <input type="text" name="email">
+                    </label>
+
+                    <input type="submit" value="Subscribe!">
+                </form>
+            </body>
+        </html>
+    "#,
+    )
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+pub struct Input {
+    name: String,
+    email: String,
+}
+
+pub async fn accept_form(Form(input): Form<Input>) -> Html<&'static str> {
+    tracing::debug!("form input {:?}", input);
+    Html("<h3>Form posted</h3>")
+}
+pub async fn accept_json(Json(input): Json<Input>) -> Html<&'static str> {
+    tracing::debug!("json params {:?}", input);
+    Html("<h3>Json posted x1</h3>")
+}
+pub async fn res_json(Json(input): Json<Input>) -> impl IntoResponse {
+    tracing::debug!("json params {:?}", input);
+    Json(json!({
+        "result": "ok",
+        "number": 1,
+    }))
+}
+
+pub async fn handler_404() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "Nothing to see here!")
+}
--- 21-axumapp/src/main.rs
+++ 21-axumapp/src/main.rs
@@ -1,28 +1,15 @@
 use axum::{
-    extract::Query,
-    response::{Html, IntoResponse},
     routing::{get, post},
-    Form, Json, Router,
+    Router,
 };
-use serde::Deserialize;
-use serde_json::json;
-use tower_http::{
-    services::{ServeDir, ServeFile},
-    trace::TraceLayer,
-};
-#[derive(Debug, Deserialize)]
-#[allow(dead_code)]
-struct Params {
-    foo: i32,
-    bar: String,
-    aa: Option<i32>,
-}
+mod handlers;
+use handlers::{accept_form, accept_json, handler, handler_404, query, res_json, show_form};
+use tower_http::{services::ServeDir, trace::TraceLayer};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let serve_dir = ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
     // build our application with a route
     let app = Router::new()
         .route("/", get(handler))
@@ -32,8 +19,8 @@ async fn main() {
         .route("/resjson", post(res_json))
         .nest_service("/assets2", ServeDir::new("assets2/test.html"))
         .nest_service("/assets3", ServeDir::new("assets3"))
-        .fallback_service(serve_dir)
-        .layer(TraceLayer::new_for_http());
+        .layer(TraceLayer::new_for_http())
+        .fallback(handler_404);
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -42,63 +29,4 @@ async fn main() {
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
 
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
-}
-
-async fn query(Query(params): Query<Params>) -> impl IntoResponse {
-    tracing::debug!("query params {:?}", params);
-    Html("<h3>Test query</h3>")
-}
-
-async fn show_form() -> Html<&'static str> {
-    Html(
-        r#"
-        <!doctype html>
-        <html>
-            <head></head>
-            <body>
-                <form action="/form" method="post">
-                    <label for="name">
-                        Enter your name:
-                        <input type="text" name="name">
-                    </label>
-
-                    <label>
-                        Enter your email:
-                        <input type="text" name="email">
-                    </label>
-
-                    <input type="submit" value="Subscribe!">
-                </form>
-            </body>
-        </html>
-    "#,
-    )
-}
-
-#[allow(dead_code)]
-#[derive(Debug, Deserialize)]
-struct Input {
-    name: String,
-    email: String,
-}
-
-async fn accept_form(Form(input): Form<Input>) -> Html<&'static str> {
-    tracing::debug!("form input {:?}", input);
-    Html("<h3>Form posted</h3>")
-}
-async fn accept_json(Json(input): Json<Input>) -> Html<&'static str> {
-    tracing::debug!("json params {:?}", input);
-    Html("<h3>Json posted x1</h3>")
-}
-
-async fn res_json(Json(input): Json<Input>) -> impl IntoResponse {
-    tracing::debug!("json params {:?}", input);
-    Json(json!({
-        "result": "ok",
-        "number": 1,
-    }))
 }
